fda355f021125cf4c17aa5987e3081a2e285ed6d
Robert YC Park <robertpark1228@gmail.com>
2023-01-09 12:50:08 +0900

    Update spliter_by_10000000.sh

--- spliter_by_10000000.sh
+++ spliter_by_10000000.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 a=${1}
+last=$(expr ${a} - 1)
 b=10000000
 chromosome=${2}
 dividen=$(expr ${1} / ${b})
 c=$(expr ${dividen} + 1)
 
+for i in $(seq 1 ${c})
+        do
+                d=$(expr ${a} - 1)
+                a=$(expr ${a} - ${b})
+                if [ ${a} -lt 1 ]
+                        then
+                                a=1
+                fi
 
-for i in $(seq 1 ${c});do d=${a};a=$(expr ${a} - ${b});if [ ${a} -lt 1 ];then a=1;fi;echo ${chromosome}:${a}-${chromosome}:${d};done
+
+                if [ ${d} -eq ${last} ]
+                        then
+
+                                d=$(expr ${d} + 1)
+
+                fi
+
+        echo ${chromosome}:${a}-${d}
+done
